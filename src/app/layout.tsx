1623f399ffa3466dbfad3ca7a082757b13528699
mgo <maxime.goossens@gmail.com>
2023-03-20 22:24:41 +0000

    Remove injected theme javascript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script"
 import "../styles/tailwind.css"
 
 type Props = {
@@ -7,55 +6,14 @@ type Props = {
 
 const RootLayout: React.FC<Props> = ({ children }) => {
   return (
-    <html className="dark antialiased [font-feature-settings:'ss01']" lang="en">
-      <Script id="darkmode" dangerouslySetInnerHTML={{ __html: themeScript }} />
-
-      <body className="bg-white dark:bg-slate-900 ">{children}</body>
+    <html
+      className="dark antialiased [font-feature-settings:'ss01']"
+      lang="en"
+      data-theme="system"
+    >
+      <body className="bg-white dark:bg-slate-900">{children}</body>
     </html>
   )
 }
 
 export default RootLayout
-
-//
-// Utils
-//
-
-// TODO: improve implementation
-const themeScript = `
-  let isDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
-
-  function updateTheme(theme) {
-    theme = theme ?? window.localStorage.theme ?? 'system'
-
-    if (theme === 'dark' || (theme === 'system' && isDarkMode.matches)) {
-      document.documentElement.classList.add('dark')
-    } else if (theme === 'light' || (theme === 'system' && !isDarkMode.matches)) {
-      document.documentElement.classList.remove('dark')
-    }
-
-    return theme
-  }
-
-  function updateThemeWithoutTransitions(theme) {
-    updateTheme(theme)
-    document.documentElement.classList.add('[&_*]:!transition-none')
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
-  }
-
-  document.documentElement.setAttribute('data-theme', updateTheme())
-
-  new MutationObserver(([{ oldValue }]) => {
-    let newValue = document.documentElement.getAttribute('data-theme')
-    if (newValue !== oldValue) {
-      try {
-        window.localStorage.setItem('theme', newValue)
-      } catch {}
-      updateThemeWithoutTransitions(newValue)
-    }
-  }).observe(document.documentElement, { attributeFilter: ['data-theme'], attributeOldValue: true })
-
-  isDarkMode.addEventListener('change', () => updateThemeWithoutTransitions('dark'))
-`
